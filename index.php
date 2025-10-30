9cbc4dd6f81c60f76860d5d151bfdb42b5eb77c2
Jarif Foysal <jarifsudin6@gmail.com>
2025-10-30 21:19:15 +0600

    f

--- index.php
+++ index.php
@@ -31,6 +31,31 @@ require_once './components/header.php';
         <div class="row">
             <div class="col-md-12">
                 <h2 class="text-center text-primary my-4">Feature products</h2>
+                <div class="row">
+                    <?php
+                    // select random 4 products from the database
+                    $featuredProductsQuery = "SELECT * FROM products ORDER BY RAND() LIMIT 4";
+                    $featuredProductsResult = $conn->query($featuredProductsQuery);
+                    while ($product = $featuredProductsResult->fetch_object()):
+                    ?>
+                        <div class="col-md-3">
+                            <div class="card mb-4 h-100">
+                                <img src="./assets/images/products/<?= $product->image ?>" alt="<?= $product->name ?>"
+                                    class="card-img-top img-thumbnail p-2"
+                                    style="height: 100%; height: 200px; object-fit: contain;">
+                                <div class="card-body">
+                                    <h5 class="card-title text-truncate"
+                                        style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                                        <?= $product->name ?>
+                                    </h5>
+                                    <p class="card-text">Price: $<?= number_format($product->sale_price) ?></p>
+                                    <a href="./single-product.php?id=<?= $product->id ?>" class="btn btn-primary">View
+                                        Details</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endwhile; ?>
+                </div>
             </div>
         </div>
 
@@ -38,6 +63,31 @@ require_once './components/header.php';
         <div class="row">
             <div class="col-md-12">
                 <h2 class="text-center text-primary my-4">New arrivals</h2>
+                <div class="row">
+                    <?php
+                    // select latest 4 products from the database
+                    $newArrivalQuery = "SELECT * FROM products ORDER BY created_at DESC LIMIT 4";
+                    $newArrivalResult = $conn->query($newArrivalQuery);
+                    while ($product = $newArrivalResult->fetch_object()):
+                    ?>
+                        <div class="col-md-3">
+                            <div class="card mb-4 h-100">
+                                <img src="./assets/images/products/<?= $product->image ?>" alt="<?= $product->name ?>"
+                                    class="card-img-top img-thumbnail p-2"
+                                    style="height: 100%; height: 200px; object-fit: contain;">
+                                <div class="card-body">
+                                    <h5 class="card-title text-truncate"
+                                        style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                                        <?= $product->name ?>
+                                    </h5>
+                                    <p class="card-text">Price: $<?= number_format($product->sale_price) ?></p>
+                                    <a href="./single-product.php?id=<?= $product->id ?>" class="btn btn-primary">View
+                                        Details</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endwhile; ?>
+                </div>
             </div>
         </div>
 
